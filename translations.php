14242bd7fc8a56e6b09ed6295009a40cf4d74fcc
SlushUwU <105487011+SlushUwU@users.noreply.github.com>
2022-11-06 22:48:24 +0100

    Inital Upload

---
+++ translations.php
@@ -0,0 +1,15 @@
+<?php
+////////////////////////////////////////////////////
+//  Copyright by: Satowa Network                  //
+//  Author: Kirihito                              //
+//  Discord: https://discord.satowa-network.at    //
+//  Email: support@satowa-network.at              //
+//  Website: https://satowa-network.at            //
+////////////////////////////////////////////////////
+
+namespace satowa_network\translation;
+
+class translations
+{
+
+}
